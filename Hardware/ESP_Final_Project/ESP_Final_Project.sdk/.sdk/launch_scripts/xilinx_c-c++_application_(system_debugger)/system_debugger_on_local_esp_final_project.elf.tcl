b3622eace9ff23886258da1c57f7b8a7c2616478
Vatsa <srivatsa@pdx.edu>
2016-12-05 21:26:55 -0800

    Adding Hardware code

---
+++ Hardware/ESP_Final_Project/ESP_Final_Project.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_on_local_esp_final_project.elf.tcl
@@ -0,0 +1,9 @@
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744868A"} -index 0
+loadhw C:/Users/Vatsa/ESP_Final_Project/ESP_Final_Project.sdk/n4fpga_hw_platform_0/system.hdf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744868A"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744868A"} -index 0
+dow C:/Users/Vatsa/ESP_Final_Project/ESP_Final_Project.sdk/ESP_FINAL_PROJECT/Debug/ESP_FINAL_PROJECT.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292744868A"} -index 0
+con
